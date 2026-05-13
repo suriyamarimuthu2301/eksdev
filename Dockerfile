f2bd4ffcfc843954d6e674795a60f4af12f063c8
suriyamarimuthu2301 <suriyamarimutu2301@gmail.com>
2026-05-13 21:07:28 +0530

    my new commit

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,6 @@ COPY . /app
 
 RUN pip install -r requirements.txt
 
+EXPOSE 5000
+
 CMD ["python", "eks-dev.py"]
--- eks-dev.py
+++ eks-dev.py
@@ -1,1 +1,10 @@
-print("Hi Suriya, welcome to the EKS dev environment..")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hi Suriya, welcome to the EKS dev environment.."
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
