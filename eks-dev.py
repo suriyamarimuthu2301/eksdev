1b9c2a7c1c4a7d83c86563cbf3f18c1bd7a9ff34
suriyamarimuthu2301 <suriyamarimutu2301@gmail.com>
2026-05-13 22:06:45 +0530

    my new commitza

--- eks-dev.py
+++ eks-dev.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hi Suriya, welcome to the EKS dev environment....."
+    return "Hi Suriya, welcome to the EKS dev environment......"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
